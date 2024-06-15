3e159355cb475bec001ee2dffac83f5170652dcc
Yannick Funk <yannickfunk@yahoo.de>
2024-06-15 11:42:17 +0200

    Add patchtst

---
+++ experiments/sinus_peak_noise_patch_tst/sinus_peak_noise_patch_tst.py
@@ -0,0 +1,91 @@
+import os
+import random
+
+import numpy as np
+from lightning.pytorch.loggers import TensorBoardLogger
+from matplotlib import pyplot as plt
+from neuralforecast.losses.pytorch import DistributionLoss
+from neuralforecast.models import PatchTST
+from scipy.signal import find_peaks
+from synthetictime.simple_time_series import SimpleTimeSeries
+
+from common.utils import plot_attributions
+from nf_ti_adapter.patch_tst import PatchTstNfTiAdapter
+
+os.environ["NIXTLA_ID_AS_COL"] = "1"
+
+HORIZON = 48
+LEVELS = [80, 90]
+INPUT_SIZE = 2 * HORIZON
+TRAIN_SPLIT = 0.8
+VAL_SPLIT = 0.2
+
+model = PatchTST(
+    input_size=INPUT_SIZE,
+    h=HORIZON,
+    loss=DistributionLoss(distribution="Normal", level=LEVELS, return_params=True),
+    # loss=MQLoss(level=LEVELS),
+    max_steps=2000,
+    random_seed=40,
+    early_stop_patience_steps=5,
+    logger=TensorBoardLogger("logs"),
+)
+
+nf_ti_adapter = PatchTstNfTiAdapter(model, 1)
+time_series = SimpleTimeSeries(
+    size=1000,
+    base_amplitude=2.0,
+    base_frequency=0.05,
+    base_noise_scale=0,
+    base_noise_amplitude=0.5,
+).synthetic_time_series
+time_series_ds = np.arange(len(time_series))
+
+peaks, _ = find_peaks(time_series)
+for peak in peaks:
+    # skip peak with a probability
+    if random.random() < 0.8:
+        continue
+    sub_arr = time_series[peak - 3 : peak + 3]
+    sub_arr += np.random.normal(0, 2, len(sub_arr))
+
+# train test split
+last_train_idx = int(len(time_series) * TRAIN_SPLIT)
+train_y = time_series[:last_train_idx]
+train_ds = time_series_ds[:last_train_idx]
+
+test_y = time_series[last_train_idx:]
+test_ds = time_series_ds[last_train_idx:]
+
+# plot time series with train / test split borders as vertical lines
+plt.plot(time_series)
+plt.axvline(x=last_train_idx, color="r", label="train / test split", linestyle="--")
+plt.legend()
+plt.title("Time Series")
+plt.show()
+
+nf_ti_adapter.fit(train_ds, train_y, val_size=int(VAL_SPLIT * len(train_y)))
+
+# pick random INPUT_SIZE window from test set
+# start_idx = random.randint(0, len(test_ds) - INPUT_SIZE)
+start_idx = 98
+test_input_ds = test_ds[start_idx : start_idx + INPUT_SIZE]
+test_input_y = test_y[start_idx : start_idx + INPUT_SIZE]
+
+predictions = nf_ti_adapter.predict(
+    ds=test_input_ds, y=test_input_y  # , test_ds=test_ds, test_y=test_y
+)
+
+target_indices = list(range(len(predictions[f"{model}-loc"])))
+attribution_list = nf_ti_adapter.explain("TIG", target_indices, "-loc")
+
+plot_attributions(
+    attribution_list, "-loc", test_input_ds, test_input_y, predictions, model, "TIG"
+)
+
+target_indices = list(range(len(predictions[f"{model}-scale"])))
+attribution_list = nf_ti_adapter.explain("TIG", target_indices, "-scale")
+
+plot_attributions(
+    attribution_list, "-scale", test_input_ds, test_input_y, predictions, model, "TIG"
+)
---
+++ nf_ti_adapter/patch_tst.py
@@ -0,0 +1,27 @@
+from typing import Union
+
+import pandas as pd
+import torch
+
+from nf_ti_adapter.base import Model, NfTiAdapter
+
+
+class PatchTstNfTiAdapter(NfTiAdapter):
+    def __init__(self, model: Model, freq: Union[str, int]):
+        if str(model) != "PatchTST":
+            raise ValueError("Model must be an instance of PatchTST")
+        super().__init__(model, freq)
+
+    def _forward_function(self, inputs: torch.Tensor, output_name: str):
+        output_index = self.output_names.index(output_name)
+        inputs = inputs[0, :, 0]
+        masks = torch.ones_like(inputs)
+        inputs = torch.unsqueeze(torch.vstack([inputs, masks]), 0)
+        batch = {
+            "temporal": inputs,
+            "temporal_cols": pd.Index(["y", "available_mask"]),
+            "y_idx": 0,
+        }
+        batch_idx = 0
+        model_output = self.model.predict_step(batch, batch_idx)
+        return model_output[0, :, output_index]
