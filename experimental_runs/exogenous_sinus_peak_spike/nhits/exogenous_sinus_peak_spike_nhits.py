8e57432ebd004f7270fd7bb72db8b940d73799d1
Yannick Funk <yannickfunk@yahoo.de>
2024-09-27 12:22:59 +0200

    Restructure exogenous sinus peak spike

---
+++ experimental_runs/exogenous_sinus_peak_spike/nhits/exogenous_sinus_peak_spike_nhits.py
@@ -0,0 +1,26 @@
+from lightning.pytorch.loggers import TensorBoardLogger
+from neuralforecast.losses.pytorch import DistributionLoss
+from neuralforecast.models import NHITS
+
+from experiment_templates.exogenous_sinus_peak_spike import (HORIZON,
+                                                             INPUT_SIZE,
+                                                             LEVELS, run)
+from nf_ti_adapter.nhits import NhitsNfTiAdapter
+
+model = NHITS(
+    input_size=INPUT_SIZE,
+    h=HORIZON,
+    loss=DistributionLoss(distribution="Normal", level=LEVELS, return_params=True),
+    # loss=MQLoss(level=LEVELS),
+    max_steps=1700,
+    random_seed=40,
+    # early_stop_patience_steps=5,
+    logger=TensorBoardLogger("logs"),
+    hist_exog_list=["sine_spiky_a", "sine_spiky_b"],
+    scaler_type="robust",
+    # exclude_insample_y=True,
+)
+
+adapter = NhitsNfTiAdapter(model, 1)
+
+run(model, adapter)
