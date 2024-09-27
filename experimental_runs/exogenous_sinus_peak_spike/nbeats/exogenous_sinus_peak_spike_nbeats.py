7ca4834b0b51ccea1f8abe9151f42ddb207fca86
Yannick Funk <yannickfunk@yahoo.de>
2024-09-27 12:43:30 +0200

    Add nbeats exogenous sinus peak spike

---
+++ experimental_runs/exogenous_sinus_peak_spike/nbeats/exogenous_sinus_peak_spike_nbeats.py
@@ -0,0 +1,29 @@
+from lightning.pytorch.loggers import TensorBoardLogger
+from neuralforecast.losses.pytorch import DistributionLoss
+from neuralforecast.models import NBEATSx
+
+from experiment_templates.exogenous_sinus_peak_spike import (
+    HORIZON,
+    INPUT_SIZE,
+    LEVELS,
+    run,
+)
+from nf_ti_adapter.nhits import NhitsNfTiAdapter
+
+model = NBEATSx(
+    input_size=INPUT_SIZE,
+    h=HORIZON,
+    loss=DistributionLoss(distribution="Normal", level=LEVELS, return_params=True),
+    # loss=MQLoss(level=LEVELS),
+    max_steps=1700,
+    random_seed=40,
+    # early_stop_patience_steps=5,
+    logger=TensorBoardLogger("logs"),
+    hist_exog_list=["sine_spiky_a", "sine_spiky_b"],
+    scaler_type="robust",
+    # exclude_insample_y=True,
+)
+
+adapter = NhitsNfTiAdapter(model, 1)
+
+run(model, adapter)
