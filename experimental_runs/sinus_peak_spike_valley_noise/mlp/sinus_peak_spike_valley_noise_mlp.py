8acc96eb0cdc045914c2091a88ac5908cccfbb07
Yannick Funk <yannickfunk@yahoo.de>
2024-10-10 14:39:55 +0200

    Add sinus peak spike valley noise pdf plots

--- experimental_runs/sinus_peak_spike_valley_noise/mlp/sinus_peak_spike_valley_noise_mlp.py
+++ experimental_runs/sinus_peak_spike_valley_noise/mlp/sinus_peak_spike_valley_noise_mlp.py
@@ -2,12 +2,9 @@ from lightning.pytorch.loggers import TensorBoardLogger
 from neuralforecast.losses.pytorch import DistributionLoss
 from neuralforecast.models import MLP
 
-from experiment_templates.sinus_peak_spike_valley_noise import (
-    HORIZON,
-    INPUT_SIZE,
-    LEVELS,
-    run,
-)
+from experiment_templates.sinus_peak_spike_valley_noise import (HORIZON,
+                                                                INPUT_SIZE,
+                                                                LEVELS, run)
 from nf_ti_adapter.nhits import NhitsNfTiAdapter
 
 model = MLP(
@@ -16,7 +13,7 @@ model = MLP(
     h=HORIZON,
     loss=DistributionLoss(distribution="Normal", level=LEVELS, return_params=True),
     # loss=MQLoss(level=LEVELS),
-    max_steps=1000,
+    max_steps=1200,
     random_seed=40,
     # early_stop_patience_steps=5,
     logger=TensorBoardLogger("logs"),
